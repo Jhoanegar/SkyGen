dfcf8c0ef7bd1e3c856396036b32167dcdfa4b97
Jhoan <jhoan-mac@hotmail.co.uk>
2013-11-20 18:39:56 -0600

    Improved grammar print

--- lib/skygen/interpreter.rb
+++ lib/skygen/interpreter.rb
@@ -22,18 +22,22 @@ class Interpreter
     move_print 8,2, "Start symbol: " 
     move_print 8,col, grammar.start_symbol.to_s
     move_print 10,2, "Production rules:" 
+    move_print 11,cols*2/3-2, "Probability"
     i = 12
     grammar.rules.each do |rule|
       output = ""
-      output << tab << "(" << rule[:id].to_s << ")" << tab << 
+      output = "(" << rule[:id].to_s << ")" << tab <<
              rule[:symbol].to_s << " " << def_symbol << " " <<
-             rule[:body].join(" ") << " " << 
-             rule[:probability].to_s << "\n"
+             rule[:body].join(" ") << " " 
+      move_print i,6, output 
+      move_print i,cols*2/3 ,rule[:probability].to_s << "\n"
       move_print i,6,output
       i += 1
    end
-    refresh
-    getch
+     message = "<Press any key to continue>"
+     move_print lines-1,cols-message.size,message
+     refresh
+     getch
   end
 
   def find_grammar(name)
